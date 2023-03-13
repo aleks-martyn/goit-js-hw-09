e8dffa7fa6ae17dbc95da399e5591d3036c46714
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-14 00:41:18 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body'); 
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtnEl.addEventListener('click', handleStartBtnClick);
+
+function handleStartBtnClick(event) {
+    timerId = setInterval(timerHandler, 1000);
+    console.log(event.target);
+}
+
+function timerHandler() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+stopBtnEl.addEventListener('click', handleStopBtnClick);
+
+function handleStopBtnClick() {
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
