c56fb7b5f5eeb0fa87194f030bd2c7a6cd4f3f82
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-14 18:01:49 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,4 @@
-const bodyEl = document.querySelector('body'); 
+const bodyEl = document.querySelector('body');
 const startBtnEl = document.querySelector('button[data-start]');
 const stopBtnEl = document.querySelector('button[data-stop]');
 let timerId = null;
@@ -7,17 +7,25 @@ startBtnEl.addEventListener('click', handleStartBtnClick);
 stopBtnEl.addEventListener('click', handleStopBtnClick);
 
 function handleStartBtnClick(event) {
-    timerId = setInterval(timerHandler, 1000);
-    event.target.setAttribute('disabled', 'true');
-    console.log(event.target);
+  timerId = setInterval(timerHandler, 1000);
+  event.target.setAttribute('disabled', 'true');
+
+  if (stopBtnEl.hasAttribute('disabled')) {
+    stopBtnEl.removeAttribute('disabled');
+  }
+}
+
+function handleStopBtnClick(event) {
+  clearInterval(timerId);
+  event.target.setAttribute('disabled', 'true');
+
+  if (startBtnEl.hasAttribute('disabled')) {
+    startBtnEl.removeAttribute('disabled');
+  }
 }
 
 function timerHandler() {
-    bodyEl.style.backgroundColor = getRandomHexColor();
-}
-
-function handleStopBtnClick() {
-    clearInterval(timerId);
+  bodyEl.style.backgroundColor = getRandomHexColor();
 }
 
 function getRandomHexColor() {
