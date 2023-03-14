f18e3daadf0a733ade80a67ea3e7421c1fe02694
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-14 19:22:09 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,17 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
 const inputEl = document.querySelector('#datetime-picker');
 const buttonEl = document.querySelector('button[data-start]');
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(inputEl, options);
