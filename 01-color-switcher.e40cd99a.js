ae09bce51d1d40783c6d3bb09c288b0cd4685d33
aleks-martyn <115187534+aleks-martyn@users.noreply.github.com>
2023-03-14 16:02:18 +0000

    Deploying to gh-pages from @ aleks-martyn/goit-js-hw-09@c56fb7b5f5eeb0fa87194f030bd2c7a6cd4f3f82 🚀

--- 01-color-switcher.e40cd99a.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let n=null;function r(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(t){n=setInterval(r,1e3),t.target.setAttribute("disabled","true"),console.log(t.target)})),o.addEventListener("click",(function(){clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.e40cd99a.js.map
