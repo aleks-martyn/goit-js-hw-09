db3c18fcaa7fc21f1e79327897616c168f504ba6
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-17 14:12:58 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,24 @@
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let position = 0;
+
+  for (let i = 0; i < amount.value; i += 1) {
+    position += 1;
+    createPromise(position, delay);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
