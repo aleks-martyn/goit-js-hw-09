6f5f204eb05308145b8f2e714fdc9f2d2dfbf3e9
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-03-17 17:52:16 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const formEl = document.querySelector('.form');
 
 formEl.addEventListener('submit', handleSubmit);
@@ -17,10 +19,10 @@ function handleSubmit(event) {
 
     createPromise(position, delayNum)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
 
     delayNum += stepNum;
